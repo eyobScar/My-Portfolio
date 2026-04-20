affbfce1862d175658193a15d59e9caf1f3f1b5d
Eyob Alayu <eyobalayu15@gmail.com>
2026-04-20 16:26:25 +0300

    react init

---
+++ portfolio/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Navbar = () => {
+  const Menu = [
+    { id: 1, name: "Home" },
+    { id: 2, name: "About" },
+    { id: 3, name: "Tech Stack" },
+    { id: 4, name: "Projects" },
+    { id: 5, name: "Contact Us" },
+  ];
+  return (
+    <div>navbar</div>
+  );
+};
+
+export default Navbar;
