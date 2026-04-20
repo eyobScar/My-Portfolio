ab51589073262ba41b0a196940d3627413e93639
Eyob Alayu <eyobalayu15@gmail.com>
2026-04-20 22:03:05 +0300

    base properties and assets

--- portfolio/src/Components/Navbar/Navbar.jsx
+++ portfolio/src/Components/Navbar/Navbar.jsx
@@ -9,7 +9,119 @@ const Navbar = () => {
     { id: 5, name: "Contact Us" },
   ];
   return (
-    <div>navbar</div>
+    <div className="overflow-auto ">
+      <p className="h-[4000px] ">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, quaerat
+        deserunt? Ex officiis odit soluta pariatur, dolorem quo, vel doloremque
+        tempore veniam eum sed commodi. Consequuntur labore laudantium enim aut
+        blanditiis. Perspiciatis doloremque dolorem quae ut quam sint, dolor
+        beatae adipisci cupiditate ipsa sapiente praesentium consequatur aut
+        expedita possimus recusandae maxime impedit quidem at id laborum? Fuga
+        blanditiis sit perspiciatis doloribus sunt commodi architecto sapiente
+        aliquam rem? Non vitae ratione praesentium culpa nisi sint? Odit
+        consequuntur earum tempore, iste amet molestias aperiam officia, vero
+        qui modi accusantium eum impedit vel voluptas debitis assumenda.
+        Molestias neque non natus recusandae culpa adipisci exercitationem
+        nihil. Repellendus perferendis consequuntur corrupti similique optio
+        expedita accusamus odit laborum neque, repellat repudiandae ab fugiat!
+        Vitae repudiandae deleniti aperiam numquam culpa quae incidunt commodi
+        placeat maiores soluta ut perspiciatis dicta voluptate quia libero qui
+        aliquam error quam voluptates, sint pariatur eos voluptatum doloribus.
+        Numquam vel perferendis quod, voluptatum fugiat quas dolor sed laborum.
+        Libero quis facere eius maxime omnis labore suscipit praesentium dolorum
+        blanditiis! Voluptates accusantium quas nostrum, repellat ullam error
+        minima sequi commodi provident suscipit culpa quod consequatur magni
+        ipsam ut, veritatis sed eius sunt neque pariatur quae, quo debitis quam
+        esse. Dolor, excepturi. Adipisci harum recusandae, esse iure vel animi
+        mollitia vitae iste ratione reprehenderit obcaecati vero expedita eos
+        non eaque molestias. Provident, maxime dicta repellendus consequuntur ab
+        consequatur asperiores eius iusto dolorum sapiente nihil perspiciatis
+        eos recusandae quos odio, atque quidem nostrum fuga culpa quis explicabo
+        quibusdam excepturi. A molestiae itaque obcaecati tempora qui magni
+        blanditiis iure reprehenderit voluptas ratione quam doloremque enim
+        tempore sequi, dolorem rerum quod impedit nulla consequuntur natus
+        cupiditate sint sed? Sapiente architecto ipsam debitis reprehenderit
+        distinctio, culpa rerum eligendi libero vitae ad ab quisquam nesciunt
+        dolores. Accusamus et unde rem quae sunt, voluptate soluta beatae
+        expedita laboriosam, quam odio in nisi maxime voluptates. Quia
+        blanditiis aliquid similique numquam perspiciatis corporis impedit quis
+        nulla molestiae, earum facilis deserunt sunt ullam officiis excepturi
+        esse illum ipsum ad temporibus. Quas expedita voluptatem hic animi
+        consequuntur fugit molestias sint dicta, at dignissimos, odit adipisci
+        ex qui quidem. Corrupti aliquid est debitis maxime provident illum,
+        harum dolorem cumque magni laboriosam soluta doloremque delectus officia
+        voluptate quasi nostrum omnis hic ipsam veritatis facilis tempora iusto
+        libero quam. Ut dolores maiores quam sequi a voluptates odio id nobis,
+        provident, aut iusto esse. Commodi perferendis optio dolorem quis,
+        blanditiis dolor, perspiciatis aspernatur debitis aperiam dicta quae
+        omnis! Amet corrupti cumque ut quibusdam. Id minima sapiente dolores?
+        Voluptates nostrum quaerat, deleniti deserunt similique esse blanditiis
+        fugit accusantium nesciunt! Maxime voluptates unde molestias natus dolor
+        possimus distinctio? Voluptatum aliquid voluptate molestiae dicta. Quos
+        illo consequuntur nemo facere omnis, maxime eos, quaerat doloribus
+        architecto blanditiis ipsa, officia expedita. Sit, nam! Dolorum alias
+        veritatis blanditiis, fuga neque quam sed voluptate accusantium, quae,
+        qui minima? Veniam sunt illo, cupiditate accusantium excepturi eum natus
+        doloremque! Provident, deserunt et deleniti pariatur necessitatibus
+        laboriosam ratione hic culpa. In dolores quisquam obcaecati est odio
+        fugiat nam dicta aliquam eum dolore atque ex cum nulla earum placeat
+        quia quo aut similique voluptatibus, numquam quaerat possimus, neque
+        porro dignissimos. Quasi illum sequi praesentium sint perspiciatis ad
+        tempore id est enim, consequuntur atque cum ipsam dolor, explicabo
+        tempora deserunt voluptates aliquam fugiat! Quasi fuga impedit doloribus
+        possimus autem exercitationem aperiam eum, libero expedita iste facere
+        quis eligendi placeat fugiat recusandae, vel totam ipsum modi dicta.
+        Iusto aut in itaque dignissimos perferendis. Aspernatur inventore libero
+        nemo deleniti omnis autem eos! Tenetur, nemo asperiores ex quibusdam ad
+        soluta dolorem, facilis facere iste eum doloremque architecto maiores!
+        Rem commodi asperiores magni amet dicta exercitationem repellendus
+        explicabo beatae quisquam reiciendis temporibus eum totam veniam autem,
+        facere est ratione maiores hic voluptatum ut! Enim atque explicabo neque
+        aliquam. Recusandae id omnis, consequuntur rerum tempore at blanditiis.
+        Facere, quisquam saepe temporibus voluptates in impedit enim repudiandae
+        dolore neque labore facilis laborum voluptatibus ut, eos, voluptatum
+        sapiente eum. Eligendi eos sequi animi aspernatur ratione quam libero
+        necessitatibus error. Ullam, molestiae obcaecati sed amet blanditiis
+        architecto repellat alias quas quo odit iusto deserunt autem inventore
+        eum. Cum quidem, odit optio, beatae dolor ipsa aut voluptate vero
+        explicabo, similique tempora soluta illum numquam. Libero consequuntur
+        magnam voluptate incidunt aut, asperiores debitis mollitia, nemo iste
+        delectus, nostrum ea nesciunt non id aspernatur est! Iure, similique
+        consequuntur doloribus dolor voluptatum labore in repellendus aut natus
+        modi enim quia velit eum maiores quasi exercitationem at beatae libero
+        debitis delectus nostrum voluptatem. Necessitatibus cum perferendis illo
+        quod hic, doloribus praesentium blanditiis doloremque, aut aliquam
+        facere unde fuga at recusandae. Numquam, ratione cupiditate. Obcaecati
+        ratione a qui praesentium natus totam neque fugit, cupiditate, modi quis
+        exercitationem officia perspiciatis non, ut ipsam eos dignissimos. Quam
+        nemo at saepe perferendis corrupti porro est earum incidunt, aliquid
+        laboriosam ipsam distinctio. Voluptatem porro ipsa qui illum, obcaecati
+        corrupti vitae debitis aspernatur. Pariatur atque officia sunt, sapiente
+        ullam voluptatibus ex dolor illo quaerat odit earum ea velit
+        perspiciatis numquam unde fugit veritatis magnam illum necessitatibus
+        nulla. Autem reprehenderit ipsum aut deleniti voluptas, asperiores
+        libero ducimus facilis ipsa tenetur voluptatibus consequatur! Dolorem,
+        dolor unde iusto voluptatem aut officia repudiandae adipisci inventore
+        praesentium accusantium porro vitae perferendis quo libero similique
+        quis molestias! Corporis nemo cupiditate fugit nam, voluptatum qui optio
+        libero voluptas iste corrupti repudiandae, commodi sapiente soluta
+        quisquam vel laborum laudantium at odit unde facere beatae? Vel ad quae
+        at veritatis aperiam necessitatibus perferendis, quos harum perspiciatis
+        culpa dolores sapiente, quidem laboriosam tempore pariatur nam? Iusto,
+        officia debitis. Nulla vero quidem alias itaque fugit asperiores
+        possimus nihil vitae! Maiores deleniti fugiat ducimus aliquid, dicta a
+        quibusdam autem rem libero odio, impedit cum nisi assumenda molestias
+        vel similique earum quisquam consequatur facere eum ullam quod vero!
+        Excepturi iusto corrupti enim temporibus facilis unde nostrum at omnis.
+        Recusandae provident, nobis, ab molestiae quisquam hic maxime quaerat
+        alias sunt deleniti excepturi totam corrupti perspiciatis voluptatibus.
+        Aspernatur labore laborum nam ducimus unde mollitia, aliquid in ex
+        doloremque rem similique, nihil nisi, blanditiis quis beatae qui dolores
+        ipsum iure sapiente! Delectus, repellat aperiam? Ratione officiis
+        aperiam quasi doloremque mollitia nostrum et eligendi eius accusantium
+        dolores.
+      </p>
+    </div>
   );
 };
 
---
+++ portfolio/src/Data/projects.js
@@ -0,0 +1,57 @@
+export const projects = [
+  {
+    id: 1,
+    title: "Food Delivery",
+    description:
+      "I built a full-stack food delivery web app where users can easily browse menus, add items to their cart, and place orders with secure online payments. It also includes an admin dashboard to manage orders, users, and menu items. This project gave me hands-on experience building everything end-to-end—from designing the frontend UI to developing backend APIs and working with a database.",
+    image: "/assets/food_delivery.png",
+    category: "Full Stack",
+    techs: ["React", "Node.js", "MongoDB", " Stripe"],
+    liveURL: "https://",
+    githubURL: "https://github.com",
+  },
+  {
+    id: 1,
+    title: "Food Delivery",
+    description:
+      "I built a full-stack food delivery web app where users can easily browse menus, add items to their cart, and place orders with secure online payments. It also includes an admin dashboard to manage orders, users, and menu items. This project gave me hands-on experience building everything end-to-end—from designing the frontend UI to developing backend APIs and working with a database.",
+    image: "/assets/food_delivery.png",
+    category: "Full Stack",
+    techs: ["React", "Node.js", "MongoDB", " Stripe"],
+    liveURL: "https://",
+    githubURL: "https://github.com",
+  },
+  {
+    id: 1,
+    title: "Food Delivery",
+    description:
+      "I built a full-stack food delivery web app where users can easily browse menus, add items to their cart, and place orders with secure online payments. It also includes an admin dashboard to manage orders, users, and menu items. This project gave me hands-on experience building everything end-to-end—from designing the frontend UI to developing backend APIs and working with a database.",
+    image: "/assets/food_delivery.png",
+    category: "Full Stack",
+    techs: ["React", "Node.js", "MongoDB", " Stripe"],
+    liveURL: "https://",
+    githubURL: "https://github.com",
+  },
+  {
+    id: 1,
+    title: "Food Delivery",
+    description:
+      "I built a full-stack food delivery web app where users can easily browse menus, add items to their cart, and place orders with secure online payments. It also includes an admin dashboard to manage orders, users, and menu items. This project gave me hands-on experience building everything end-to-end—from designing the frontend UI to developing backend APIs and working with a database.",
+    image: "/assets/food_delivery.png",
+    category: "Full Stack",
+    techs: ["React", "Node.js", "MongoDB", " Stripe"],
+    liveURL: "https://",
+    githubURL: "https://github.com",
+  },
+  {
+    id: 1,
+    title: "Food Delivery",
+    description:
+      "I built a full-stack food delivery web app where users can easily browse menus, add items to their cart, and place orders with secure online payments. It also includes an admin dashboard to manage orders, users, and menu items. This project gave me hands-on experience building everything end-to-end—from designing the frontend UI to developing backend APIs and working with a database.",
+    image: "/assets/food_delivery.png",
+    category: "Full Stack",
+    techs: ["React", "Node.js", "MongoDB", " Stripe"],
+    liveURL: "https://",
+    githubURL: "https://github.com",
+  },
+];
